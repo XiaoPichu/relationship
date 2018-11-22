e1d7b7a4a7fa8be78c567148adbecdc43b04dbd0
XiaoPichu <32864753+XiaoPichu@users.noreply.github.com>
2018-11-22 10:10:08 +0800

    Update mxnettrain.py

--- mxnettrain.py
+++ mxnettrain.py
@@ -6,6 +6,27 @@
 '''
 from __future__ import print_function
 from random import shuffle
+import logging
+
+# 第一步，创建一个logger
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)  # Log等级总开关
+# 第二步，创建一个handler，用于写入日志文件
+logfile = 'logging.log'
+fh = logging.FileHandler(logfile, mode='w')
+fh.setLevel(logging.DEBUG)  # 输出到file的log等级的开关
+# 第三步，定义handler的输出格式
+formatter = logging.Formatter("%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s")
+fh.setFormatter(formatter)
+# 第四步，添加
+logger.addHandler(fh)
+# 日志
+logger.debug('this is a logger debug message')
+logger.info('this is a logger info message')
+logger.warning('this is a logger warning message')
+logger.error('this is a logger error message')
+logger.critical('this is a logger critical message')
+
 
 def splitdataset(ratio):
     with open('nois.csv','r') as df:
